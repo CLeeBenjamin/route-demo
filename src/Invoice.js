ae217b4626efeeaf93e5d15477e634598060880f
CLeeBenjamin <castonboyd@gmail.com>
2022-11-14 11:29:00 -0500

    updated: lecture-react-router

---
+++ src/Invoice.js
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+export default function Invoice() {
+  let params = useParams();
+
+  console.log(params)
+  
+  return <h2>Invoice: {params.invoiceId}</h2>;
+}
